b333f4745045e6afe153db7f179a07ac68892c52
rullinstones <administracion@hoteltirol.com.ar>
2024-12-05 16:35:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Scroll suave
+document.querySelectorAll("nav a").forEach((link) => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+// Botón Volver Arriba
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
